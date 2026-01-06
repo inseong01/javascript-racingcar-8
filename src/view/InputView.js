8975a9c675892fb073223bf92dd3bc775514ede9
inseong01 <insung940@naver.com>
2026-01-06 10:28:13 +0900

    refactor: InputView 기능 수정
    - 개행문자 삽입

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,10 +6,10 @@ export class InputView {
   }
 
   async getCarNames() {
-    return await this.getInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)')
+    return await this.getInput('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n')
   }
 
   async getRepeatNumber() {
-    return await this.getInput('시도할 횟수는 몇 회인가요?')
+    return await this.getInput('시도할 횟수는 몇 회인가요?\n')
   }
 }
